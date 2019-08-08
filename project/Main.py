16ad875212caafb3b010b2a2ba770ae1ef22f03f
Tyler Sefcik <tyler.sefcik@gmail.com>
2019-08-07 20:15:02 -0400

    eh

--- project/Main.py
+++ project/Main.py
@@ -6,7 +6,7 @@ from project import BreastCancer as bc
 from project import HouseVotes as votes
 from project import FiveFold as ff
 from project import NeuralNetwork as nn
-from project import ForwardPropagate as fp
+import numpy as np
 import sys
 
 """
@@ -14,8 +14,40 @@ This is the main driver class for Assignment#5.  @author: Tyler Sefcik
 """
 
 
+def run_feedforward_backpropagation(data, hidden_layers, learning_rate, epoch):
+    # Split the data set into 2/3 and 1/3
+    data_train = data.sample(frac=.667)
+    data_test = data.drop(data_train.index)
+
+    neural_network = nn.NeuralNetwork(train_data=data_test, test_data=data_train)
+
+    network = neural_network.new_neural_network(inputs=data_test.shape[1], hidden_layers=hidden_layers, possible_outputs=2)
+
+    neural_network.train(network=network, training_data=data_train, learning_rate=learning_rate, epoch=epoch)
+
+    prediction_list = []
+    for row in data_test.values:
+        prediction = neural_network.test(network=network, input_row=row)
+        prediction_list.append(prediction)
+
+    actual_class = []
+    for row in data_test.values:
+        actual = row[-1]
+        actual_class.append(actual)
+
+    correct = 0
+    for index in range(len(actual_class)):
+        if actual_class[index] == prediction_list[index]:
+            correct += 1
+
+    accuracy = (correct / len(actual_class)) * 100
+
+    print("Accuracy: " + str(accuracy) + "%")
+    return accuracy, prediction_list
+
+
 # Iris
-def run_iris(filename, target_class, class_wanted, iris_names):
+def run_iris(filename, target_class, class_wanted, iris_names, learning_rate, epoch):
     # Setup data
     iris_obj = iris.Iris()
     iris_data = iris_obj.setup_data_iris(filename=filename, target_class=target_class, class_wanted=class_wanted,
@@ -24,19 +56,52 @@ def run_iris(filename, target_class, class_wanted, iris_names):
     # Setup five fold cross validation
     five_fold = ff.FiveFold()
     iris1, iris2, iris3, iris4, iris5 = five_fold.five_fold_sort_class(data=iris_data, sortby=target_class)
-    print(iris1)
-    iris_network = nn.NeuralNetwork(train_data=iris1, test_data=iris2)
-    iris1_network = iris_network.new_neural_network(inputs=iris1.shape[1], hidden_layers=1, possible_outputs=2)
-    iris_forward = fp.ForwardPropagate()
 
-    for row in iris1.values:
-        iris_forward.forward_propagate_network(input_row=row, network=iris1_network)
+    print("Iris 0 layers")
+    # Run 0 hidden layers
+    a01, list01 = run_feedforward_backpropagation(iris1, 0, learning_rate, epoch)
+    print("Classification on Iris 0 hidden layers fold 1:")
+    print(list01)
+    a02, list02 = run_feedforward_backpropagation(iris2, 0, learning_rate, epoch)
+    a03, list03 = run_feedforward_backpropagation(iris3, 0, learning_rate, epoch)
+    a04, list04 = run_feedforward_backpropagation(iris4, 0, learning_rate, epoch)
+    a05, list05 = run_feedforward_backpropagation(iris5, 0, learning_rate, epoch)
 
-    return iris1
+    mean0 = np.average([a01, a02, a03, a04, a05])
+    print("Mean Accuracy of Iris 0 hidden layers: " + str(mean0) + "%")
+    print()
+
+    print("Iris 1 hidden layer")
+    # Run 1 hidden layers
+    a11, list11 = run_feedforward_backpropagation(iris1, 1, learning_rate, epoch)
+    print("Classification on Iris 1 hidden layers fold 1:")
+    print(list11)
+    a12, list12 = run_feedforward_backpropagation(iris2, 1, learning_rate, epoch)
+    a13, list13 = run_feedforward_backpropagation(iris3, 1, learning_rate, epoch)
+    a14, list14 = run_feedforward_backpropagation(iris4, 1, learning_rate, epoch)
+    a15, list15 = run_feedforward_backpropagation(iris5, 1, learning_rate, epoch)
+
+    mean1 = np.average([a11, a12, a13, a14, a15])
+    print("Mean Accuracy of Iris 1 hidden layers: " + str(mean1) + "%")
+    print()
+
+    print("Iris 2 hidden layers")
+    # Run 2 hidden layers
+    a21, list21 = run_feedforward_backpropagation(iris1, 2, learning_rate, epoch)
+    print("Classification on Iris 2 hidden layers fold 1:")
+    print(list21)
+    a22, list22 = run_feedforward_backpropagation(iris2, 2, learning_rate, epoch)
+    a23, list23 = run_feedforward_backpropagation(iris3, 2, learning_rate, epoch)
+    a24, list24 = run_feedforward_backpropagation(iris4, 2, learning_rate, epoch)
+    a25, list25 = run_feedforward_backpropagation(iris5, 2, learning_rate, epoch)
+
+    mean2 = np.average([a21, a22, a23, a24, a25])
+    print("Mean Accuracy of Iris 2 hidden layers: " + str(mean2) + "%")
+    print()
 
 
 # Glass
-def run_glass(filename, target_class, class_wanted, glass_names):
+def run_glass(filename, target_class, class_wanted, glass_names, learning_rate, epoch):
     # Setup data
     glass_obj = glass.Glass()
     glass_data = glass_obj.setup_data_glass(filename=filename, target_class=target_class, class_wanted=class_wanted,
@@ -46,11 +111,51 @@ def run_glass(filename, target_class, class_wanted, glass_names):
     five_fold = ff.FiveFold()
     glass1, glass2, glass3, glass4, glass5 = five_fold.five_fold_sort_class(data=glass_data, sortby=target_class)
 
-    return glass1
+    print("Glass 0 layers")
+    # Run 0 hidden layers
+    a01, list01 = run_feedforward_backpropagation(glass1, 0, learning_rate, epoch)
+    print("Classification on Glass 0 hidden layers fold 1:")
+    print(list01)
+    a02, list02 = run_feedforward_backpropagation(glass2, 0, learning_rate, epoch)
+    a03, list03 = run_feedforward_backpropagation(glass3, 0, learning_rate, epoch)
+    a04, list04 = run_feedforward_backpropagation(glass4, 0, learning_rate, epoch)
+    a05, list05 = run_feedforward_backpropagation(glass5, 0, learning_rate, epoch)
+
+    mean0 = np.average([a01, a02, a03, a04, a05])
+    print("Mean Accuracy of Glass 0 hidden layers: " + str(mean0) + "%")
+    print()
+
+    print("Glass 1 hidden layer")
+    # Run 1 hidden layers
+    a11, list11 = run_feedforward_backpropagation(glass1, 1, learning_rate, epoch)
+    print("Classification on Glass 1 hidden layers fold 1:")
+    print(list11)
+    a12, list12 = run_feedforward_backpropagation(glass2, 1, learning_rate, epoch)
+    a13, list13 = run_feedforward_backpropagation(glass3, 1, learning_rate, epoch)
+    a14, list14 = run_feedforward_backpropagation(glass4, 1, learning_rate, epoch)
+    a15, list15 = run_feedforward_backpropagation(glass5, 1, learning_rate, epoch)
+
+    mean1 = np.average([a11, a12, a13, a14, a15])
+    print("Mean Accuracy of Glass 1 hidden layers: " + str(mean1) + "%")
+    print()
+
+    print("Glass 2 hidden layers")
+    # Run 2 hidden layers
+    a21, list21 = run_feedforward_backpropagation(glass1, 2, learning_rate, epoch)
+    print("Classification on Glass 2 hidden layers fold 1:")
+    print(list21)
+    a22, list22 = run_feedforward_backpropagation(glass2, 2, learning_rate, epoch)
+    a23, list23 = run_feedforward_backpropagation(glass3, 2, learning_rate, epoch)
+    a24, list24 = run_feedforward_backpropagation(glass4, 2, learning_rate, epoch)
+    a25, list25 = run_feedforward_backpropagation(glass5, 2, learning_rate, epoch)
+
+    mean2 = np.average([a21, a22, a23, a24, a25])
+    print("Mean Accuracy of Glass 2 hidden layers: " + str(mean2) + "%")
+    print()
 
 
 # Soybean
-def run_soybean(filename, target_class):
+def run_soybean(filename, target_class, learning_rate, epoch):
     # Setup data
     soybean_obj = soybean.Soybean()
     soybean_data = soybean_obj.setup_data_soybean(filename=filename, target_class=target_class)
@@ -60,11 +165,51 @@ def run_soybean(filename, target_class):
     soybean1, soybean2, soybean3, soybean4, soybean5 = five_fold.five_fold_sort_class(data=soybean_data,
                                                                                       sortby=target_class)
 
-    return soybean1
+    print("Soybean 0 layers")
+    # Run 0 hidden layers
+    a01, list01 = run_feedforward_backpropagation(soybean1, 0, learning_rate, epoch)
+    print("Classification on Soybean 0 hidden layers fold 1:")
+    print(list01)
+    a02, list02 = run_feedforward_backpropagation(soybean2, 0, learning_rate, epoch)
+    a03, list03 = run_feedforward_backpropagation(soybean3, 0, learning_rate, epoch)
+    a04, list04 = run_feedforward_backpropagation(soybean4, 0, learning_rate, epoch)
+    a05, list05 = run_feedforward_backpropagation(soybean5, 0, learning_rate, epoch)
+
+    mean0 = np.average([a01, a02, a03, a04, a05])
+    print("Mean Accuracy of Soybean 0 hidden layers: " + str(mean0) + "%")
+    print()
+
+    print("Soybean 1 hidden layer")
+    # Run 1 hidden layers
+    a11, list11 = run_feedforward_backpropagation(soybean1, 1, learning_rate, epoch)
+    print("Classification on Soybean 1 hidden layers fold 1:")
+    print(list11)
+    a12, list12 = run_feedforward_backpropagation(soybean2, 1, learning_rate, epoch)
+    a13, list13 = run_feedforward_backpropagation(soybean3, 1, learning_rate, epoch)
+    a14, list14 = run_feedforward_backpropagation(soybean4, 1, learning_rate, epoch)
+    a15, list15 = run_feedforward_backpropagation(soybean5, 1, learning_rate, epoch)
+
+    mean1 = np.average([a11, a12, a13, a14, a15])
+    print("Mean Accuracy of Soybean 1 hidden layers: " + str(mean1) + "%")
+    print()
+
+    print("Soybean 2 hidden layers")
+    # Run 2 hidden layers
+    a21, list21 = run_feedforward_backpropagation(soybean1, 2, learning_rate, epoch)
+    print("Classification on Soybean 2 hidden layers fold 1:")
+    print(list21)
+    a22, list22 = run_feedforward_backpropagation(soybean2, 2, learning_rate, epoch)
+    a23, list23 = run_feedforward_backpropagation(soybean3, 2, learning_rate, epoch)
+    a24, list24 = run_feedforward_backpropagation(soybean4, 2, learning_rate, epoch)
+    a25, list25 = run_feedforward_backpropagation(soybean5, 2, learning_rate, epoch)
+
+    mean2 = np.average([a21, a22, a23, a24, a25])
+    print("Mean Accuracy of Soybean 2 hidden layers: " + str(mean2) + "%")
+    print()
 
 
 # Breast Cancer
-def run_bc(filename, target_class, class_wanted, bc_names):
+def run_bc(filename, target_class, class_wanted, bc_names, learning_rate, epoch):
     # Setup data
     bc_obj = bc.BreastCancer()
     bc_data = bc_obj.setup_data_bc(filename=filename, target_class=target_class, class_wanted=class_wanted,
@@ -73,11 +218,51 @@ def run_bc(filename, target_class, class_wanted, bc_names):
     five_fold = ff.FiveFold()
     bc1, bc2, bc3, bc4, bc5 = five_fold.five_fold_sort_class(data=bc_data, sortby=target_class)
 
-    return bc1
+    print("Breast Cancer 0 layers")
+    # Run 0 hidden layers
+    a01, list01 = run_feedforward_backpropagation(bc1, 0, learning_rate, epoch)
+    print("Classification on Breast Cancer 0 hidden layers fold 1:")
+    print(list01)
+    a02, list02 = run_feedforward_backpropagation(bc2, 0, learning_rate, epoch)
+    a03, list03 = run_feedforward_backpropagation(bc3, 0, learning_rate, epoch)
+    a04, list04 = run_feedforward_backpropagation(bc4, 0, learning_rate, epoch)
+    a05, list05 = run_feedforward_backpropagation(bc5, 0, learning_rate, epoch)
+
+    mean0 = np.average([a01, a02, a03, a04, a05])
+    print("Mean Accuracy of Breast Cancer 0 hidden layers: " + str(mean0) + "%")
+    print()
+
+    print("Breast Cancer 1 hidden layer")
+    # Run 1 hidden layers
+    a11, list11 = run_feedforward_backpropagation(bc1, 1, learning_rate, epoch)
+    print("Classification on Breast Cancer 1 hidden layers fold 1:")
+    print(list11)
+    a12, list12 = run_feedforward_backpropagation(bc2, 1, learning_rate, epoch)
+    a13, list13 = run_feedforward_backpropagation(bc3, 1, learning_rate, epoch)
+    a14, list14 = run_feedforward_backpropagation(bc4, 1, learning_rate, epoch)
+    a15, list15 = run_feedforward_backpropagation(bc5, 1, learning_rate, epoch)
+
+    mean1 = np.average([a11, a12, a13, a14, a15])
+    print("Mean Accuracy of Breast Cancer 1 hidden layers: " + str(mean1) + "%")
+    print()
+
+    print("Breast Cancer 2 hidden layers")
+    # Run 2 hidden layers
+    a21, list21 = run_feedforward_backpropagation(bc1, 2, learning_rate, epoch)
+    print("Classification on Breast Cancer 2 hidden layers fold 1:")
+    print(list21)
+    a22, list22 = run_feedforward_backpropagation(bc2, 2, learning_rate, epoch)
+    a23, list23 = run_feedforward_backpropagation(bc3, 2, learning_rate, epoch)
+    a24, list24 = run_feedforward_backpropagation(bc4, 2, learning_rate, epoch)
+    a25, list25 = run_feedforward_backpropagation(bc5, 2, learning_rate, epoch)
+
+    mean2 = np.average([a21, a22, a23, a24, a25])
+    print("Mean Accuracy of Breast Cancer 2 hidden layers: " + str(mean2) + "%")
+    print()
 
 
 # House Votes
-def run_votes(filename, target_class, class_wanted, vote_names):
+def run_votes(filename, target_class, class_wanted, vote_names, learning_rate, epoch):
     # Setup data
     votes_obj = votes.HouseVotes()
     votes_data = votes_obj.setup_data_votes(filename=filename, target_class=target_class, class_wanted=class_wanted,
@@ -86,7 +271,47 @@ def run_votes(filename, target_class, class_wanted, vote_names):
     five_fold = ff.FiveFold()
     votes1, votes2, votes3, votes4, votes5 = five_fold.five_fold_sort_class(data=votes_data, sortby=target_class)
 
-    return votes1
+    print("House Votes 0 layers")
+    # Run 0 hidden layers
+    a01, list01 = run_feedforward_backpropagation(votes1, 0, learning_rate, epoch)
+    print("Classification on House Votes 0 hidden layers fold 1:")
+    print(list01)
+    a02, list02 = run_feedforward_backpropagation(votes2, 0, learning_rate, epoch)
+    a03, list03 = run_feedforward_backpropagation(votes3, 0, learning_rate, epoch)
+    a04, list04 = run_feedforward_backpropagation(votes4, 0, learning_rate, epoch)
+    a05, list05 = run_feedforward_backpropagation(votes5, 0, learning_rate, epoch)
+
+    mean0 = np.average([a01, a02, a03, a04, a05])
+    print("Mean Accuracy of House Votes 0 hidden layers: " + str(mean0) + "%")
+    print()
+
+    print("House Votes 1 hidden layer")
+    # Run 1 hidden layers
+    a11, list11 = run_feedforward_backpropagation(votes1, 1, learning_rate, epoch)
+    print("Classification on House Votes 1 hidden layers fold 1:")
+    print(list11)
+    a12, list12 = run_feedforward_backpropagation(votes2, 1, learning_rate, epoch)
+    a13, list13 = run_feedforward_backpropagation(votes3, 1, learning_rate, epoch)
+    a14, list14 = run_feedforward_backpropagation(votes4, 1, learning_rate, epoch)
+    a15, list15 = run_feedforward_backpropagation(votes5, 1, learning_rate, epoch)
+
+    mean1 = np.average([a11, a12, a13, a14, a15])
+    print("Mean Accuracy of House Votes 1 hidden layers: " + str(mean1) + "%")
+    print()
+
+    print("House Votes 2 hidden layers")
+    # Run 2 hidden layers
+    a21, list21 = run_feedforward_backpropagation(votes1, 2, learning_rate, epoch)
+    print("Classification on House Votes 2 hidden layers fold 1:")
+    print(list21)
+    a22, list22 = run_feedforward_backpropagation(votes2, 2, learning_rate, epoch)
+    a23, list23 = run_feedforward_backpropagation(votes3, 2, learning_rate, epoch)
+    a24, list24 = run_feedforward_backpropagation(votes4, 2, learning_rate, epoch)
+    a25, list25 = run_feedforward_backpropagation(votes5, 2, learning_rate, epoch)
+
+    mean2 = np.average([a21, a22, a23, a24, a25])
+    print("Mean Accuracy of House Votes 2 hidden layers: " + str(mean2) + "%")
+    print()
 
 
 # Main driver to run all algorithms on each dataset
@@ -99,27 +324,24 @@ def main():
     iris_target_class = "class"
     class_wanted = "Iris-virginica"
     iris_names = ["sepal length", "sepal width", "petal length", "petal width", "class"]
-    iris1 = run_iris(filename="data/iris.data", target_class=iris_target_class, class_wanted=class_wanted,
-                     iris_names=iris_names)
+    run_iris(filename="data/iris.data", target_class=iris_target_class, class_wanted=class_wanted,
+             iris_names=iris_names, learning_rate=.21, epoch=50)
 
-    print("Iris")
     print()
 
     ##### Glass #####
     glass_target_class = "Type of glass"
     class_wanted = 3
     glass_names = ["Id", "RI", "Na", "Mg", "Al", "Si", "K", "Ca", "Ba", "Fe", "Type of glass"]
-    glass1 = run_glass(filename="data/glass.data", target_class=glass_target_class,
-                       class_wanted=class_wanted, glass_names=glass_names)
+    run_glass(filename="data/glass.data", target_class=glass_target_class, class_wanted=class_wanted,
+              glass_names=glass_names, learning_rate=.1, epoch=30)
 
-    print("Glass")
     print()
 
     ##### Soybean #####
     soybean_target_class = "34"
-    soybean1 = run_soybean(filename="data/soybean-small.data", target_class=soybean_target_class)
+    run_soybean(filename="data/soybean-small.data", target_class=soybean_target_class, learning_rate=.1, epoch=100)
 
-    print("Soybean")
     print()
 
     ##### Breast Cancer #####
@@ -128,10 +350,9 @@ def main():
     breast_cancer_names = ["Sample code number", "Clump Thickness", "Uniformity of Cell Size",
                            "Uniformity of Cell Shape", "Marginal Adhesion", "Single Epithelial Cell Size",
                            "Bare Nuclei", "Bland Chromatin", "Normal Nucleoli", "Mitoses", "Class"]
-    bc1 = run_bc(filename="data/breast-cancer-wisconsin.data", target_class=bc_target_class,
-                 class_wanted=bc_class_wanted, bc_names=breast_cancer_names)
+    run_bc(filename="data/breast-cancer-wisconsin.data", target_class=bc_target_class,
+                 class_wanted=bc_class_wanted, bc_names=breast_cancer_names, learning_rate=.5, epoch=100)
 
-    print("Breast Cancer")
     print()
 
     ##### House Votes #####
@@ -140,9 +361,8 @@ def main():
     votes_names = ["class", "handicapped", "water", "adoption", "physician", "el-salvador", "religious",
                    "anti", "aid", "mx", "immigration", "synfuels", "education", "superfund", "crime",
                    "duty-free", "export"]
-    votes1 = run_votes(filename="data/house-votes-84.data", target_class=votes_target_class,
-                       class_wanted=votes_class_wanted, vote_names=votes_names)
-    print("House Votes")
+    run_votes(filename="data/house-votes-84.data", target_class=votes_target_class,
+                       class_wanted=votes_class_wanted, vote_names=votes_names, learning_rate=.1, epoch=50)
     print()
 
 
--- project/NeuralNetwork.py
+++ project/NeuralNetwork.py
@@ -15,15 +15,129 @@ class NeuralNetwork:
     # Pass in number of inputs (rows of data), number of hidden layers, and number of possible outputs
     def new_neural_network(self, inputs, hidden_layers, possible_outputs):
         neural_network = []
-        hidden_layer = [{"weights": [np.random.random() for index in range(inputs + 1)]} for index in range(hidden_layers)]
+        hidden_layer = [{"weights": [np.random.random() for index in range(inputs + 1)]} for index in
+                        range(hidden_layers)]
         output_layer = [{"weights": [np.random.random() for index in range(hidden_layers + 1)]} for index in
                         range(possible_outputs)]
 
         neural_network.append(hidden_layer)
         neural_network.append(output_layer)
 
-        print("Initial Neural Network:")
-        for layer in neural_network:
-            print(layer)
-
         return neural_network
+
+    # Calculate activation by returning the sum of weights multiplied by the inputs
+    def calc_activation(self, weights, inputs):
+        # Get bias from weights list
+        calculated_activation = weights[-1]
+
+        # For every entry in weights, except for the bias, calculate the sum of the weight*input value
+        for index in range(len(weights) - 1):
+            # Compute activation function
+            calculated_activation += float(weights[index]) * float(inputs[index])
+
+        return calculated_activation
+
+    # Forward propagate the network
+    def forward_propagate_network(self, input_row, network):
+        # Set row equal to variable
+        input_to_work_with = input_row
+        # Iterate through the neural network
+        for layer in network:
+            # Setup the outputs from the layer
+            updated_input = []
+            # Iterate through the layers
+            for neuron in layer:
+                # Get the calculated activation
+                calculated_activation = self.calc_activation(weights=neuron["weights"], inputs=input_to_work_with)
+                # Compute the value needed with the sigmoid function
+                neuron["output"] = 1 / (1 + np.exp(-calculated_activation))
+                updated_input.append(neuron["output"])
+
+            input_to_work_with = updated_input
+
+        return input_to_work_with
+
+        # Backpropagation algorithm that will help train our feedforward neural network
+    def backpropagation(self, network, output_pattern):
+        # Get network length
+        network_length = len(network)
+        # Iterate through network backwards since we're doing backpropagation
+        for index in reversed(range(network_length)):
+            # Get current layer and length
+            current_layer = network[index]
+            current_layer_length = len(current_layer)
+            # Keep track of errors that will be used later
+            error_list = []
+
+            # If it is the last layer in the list being iterated through backwards, get output error
+            if index == len(network) - 1:
+                # Iterate through each the layer
+                for inner_index in range(current_layer_length):
+                    # Get the neuron at this index
+                    neuron = current_layer[inner_index]
+                    # Computer error for this neuron
+                    error = output_pattern[inner_index] * neuron["output"]
+                    # Add to error list for current layer
+                    error_list.append(error)
+
+            # Otherwise get hidden layer error
+            else:
+                # Iterate through the current layer
+                for inner_index in range(current_layer_length):
+                    # Instantiate error associated here
+                    error = float(0)
+                    # Add error of each neuron at this layer together
+                    for neuron in network[index + 1]:
+                        error += neuron["weights"][inner_index] * neuron["delta"]
+                    # Add error to error list for current layer
+                    error_list.append(error)
+
+            # Iterate through neurons at the current layer
+            for inner_index in range(current_layer_length):
+                # Get neuron
+                neuron = current_layer[inner_index]
+                # Computer delta for the current neuron
+                delta = error_list[inner_index] * (neuron["output"] * (1 - neuron["output"]))
+                if delta == 0:
+                    delta = .01
+                # Save that in the dictionary
+                neuron["delta"] = delta
+
+    def update_nn_weights(self, network, learning_rate, input_row):
+        network_length = len(network)
+
+        for layer in range(network_length):
+            feature_values = input_row[:-1]
+
+            if layer != 0:
+                feature_values = []
+                for neuron in network[layer-1]:
+                    feature_values.append(neuron["output"])
+            for neuron in network[layer]:
+                fv_length = len(feature_values)
+                for inner_index in range(fv_length):
+                    neuron["weights"][inner_index] += learning_rate * neuron["delta"] * feature_values[inner_index]
+                neuron["weights"][-1] += learning_rate * neuron["delta"]
+
+    # Train the neural network given the network, data, learning rate, epoch, and number of outputs
+    def train(self, network, training_data, learning_rate, epoch):
+        # Iterate through number of cycles we specify
+        for index in range(epoch):
+            error = 0
+            # Iterate through rows in the training data
+            for inputs in training_data.values:
+                # Forward propagate the network
+                self.forward_propagate_network(input_row=inputs, network=network)
+                expected_outputs = np.zeros(training_data.shape[0])
+                expected_outputs[int(inputs[-1])] = 1
+
+                # Perform backpropagation
+                self.backpropagation(network=network, output_pattern=expected_outputs)
+                # Update neural network weights
+                self.update_nn_weights(network=network, learning_rate=learning_rate, input_row=inputs)
+
+    # We just test our remaining data using our trained network by running forward propagation on each row
+    def test(self, network, input_row):
+        # Outputs from running through trained model
+        outputs = self.forward_propagate_network(input_row=input_row, network=network)
+        return outputs.index(max(outputs))
